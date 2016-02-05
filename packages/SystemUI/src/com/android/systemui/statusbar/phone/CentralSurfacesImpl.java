fca72cc03c6d7e2f2f4a96532dc3974a8a3fd21f
Roman Birg <roman@cyngn.com>
2016-02-04 16:28:00 -0800

    SystemUI: handle camera launch gesture from keyhandler
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Feb 4 16:28:00 2016 -0800
    
        framework: move device key handler logic, fix gesture camera launch
    
        In order to make device camera gestures behave the same way as launching
        the camera by double tapping the power button, route DeviceKeyHandler
        logic through the GestureLauncherService.
    
        A callback is used in order to avoid publishing the
        GestureLauncherService into the System service registry, because the
        KeyHandler code cannot directly access services local to the system
        process.
    
        OPO-427
    
        Change-Id: I2a8860b278f65ccf31bfee151513d8a23ab746c5
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Sat Feb 13 09:08:21 2016 -0800
    
        KeyHandler: don't break old interfaces
    
        Causing bootloops on devices, don't break old interfaces in the
        framework.
    
        OPO-427
    
        Change-Id: I49473b057bc4ed4710e9092c76e41c6a8134d2de
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue Feb 16 17:22:01 2016 -0800
    
        SystemUI: handle camera launch gesture from keyhadler
    
        Includes partial reverts:
    
            Revert "KeyHandler: don't break old interfaces"
            This reverts commit 31ecbdc5fcc1263e9028107858565d951798fd93.
    
            Revert "framework: move device key handler logic, fix gesture camera launch"
            This reverts commit db1b4e6e4880be1828342be9bc468d36565a2ef0.
    
        Ref: OPO-427
    
        Change-Id: Ic15543a8f63ad4521ab0ab536d00224b0bd70f8c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Michael Bestas <mikeioannina@cyanogenmod.org>
    Date:   Sat Feb 20 00:56:47 2016 +0200
    
        SystemUI: Don't vibrate on touchscreen camera gesture
    
        * This is usually handled in device-specific keyhandler,
          where vibration is optional and has the same duration
          in all gestures
    
        Change-Id: I879671c9d6f785137d63b8b70de35f5d5020748d
    
    Change-Id: Iba9211b7d342f618c4082c34d11663d89861607a
    
    [jhonboy121]: convert if else ladder to a switch case
    
    Change-Id: I36b7e966a50d42b2ed037c53a3bfa4b39583f2f9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1470,6 +1470,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
     }
 
@@ -2536,31 +2537,52 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             if (DEBUG) Log.v(TAG, "onReceive: " + intent);
             String action = intent.getAction();
             String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
-            if (Intent.ACTION_CLOSE_SYSTEM_DIALOGS.equals(action)) {
-                KeyboardShortcuts.dismiss();
-                mRemoteInputManager.closeRemoteInputs();
-                if (mLockscreenUserManager.isCurrentProfile(getSendingUserId())) {
-                    int flags = CommandQueue.FLAG_EXCLUDE_NONE;
-                    if (reason != null) {
-                        if (reason.equals(SYSTEM_DIALOG_REASON_RECENT_APPS)) {
-                            flags |= CommandQueue.FLAG_EXCLUDE_RECENTS_PANEL;
+            switch (action) {
+                case Intent.ACTION_CLOSE_SYSTEM_DIALOGS: {
+                    KeyboardShortcuts.dismiss();
+                    mRemoteInputManager.closeRemoteInputs();
+                    if (mLockscreenUserManager.isCurrentProfile(getSendingUserId())) {
+                        int flags = CommandQueue.FLAG_EXCLUDE_NONE;
+                        if (reason != null) {
+                            if (reason.equals(SYSTEM_DIALOG_REASON_RECENT_APPS)) {
+                                flags |= CommandQueue.FLAG_EXCLUDE_RECENTS_PANEL;
+                            }
+                            // Do not collapse notifications when starting dreaming if the notifications
+                            // shade is used for the screen off animation. It might require expanded
+                            // state for the scrims to be visible
+                            if (reason.equals(SYSTEM_DIALOG_REASON_DREAM)
+                                    && mScreenOffAnimationController.shouldExpandNotifications()) {
+                                flags |= CommandQueue.FLAG_EXCLUDE_NOTIFICATION_PANEL;
+                            }
                         }
-                        // Do not collapse notifications when starting dreaming if the notifications
-                        // shade is used for the screen off animation. It might require expanded
-                        // state for the scrims to be visible
-                        if (reason.equals(SYSTEM_DIALOG_REASON_DREAM)
-                                && mScreenOffAnimationController.shouldExpandNotifications()) {
-                            flags |= CommandQueue.FLAG_EXCLUDE_NOTIFICATION_PANEL;
-                        }
+                        mShadeController.animateCollapseShade(flags);
                     }
-                    mShadeController.animateCollapseShade(flags);
+                    break;
                 }
-            } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
-                if (mNotificationShadeWindowController != null) {
-                    mNotificationShadeWindowController.setNotTouchable(false);
+                case Intent.ACTION_SCREEN_OFF: {
+                    if (mNotificationShadeWindowController != null) {
+                        mNotificationShadeWindowController.setNotTouchable(false);
+                    }
+                    finishBarAnimations();
+                    resetUserExpandedStates();
+                    break;
                 }
-                finishBarAnimations();
-                resetUserExpandedStates();
+                case Intent.ACTION_SCREEN_CAMERA_GESTURE: {
+                    boolean userSetupComplete = Settings.Secure.getInt(mContext.getContentResolver(),
+                        Settings.Secure.USER_SETUP_COMPLETE, 0) != 0;
+                    if (!userSetupComplete) {
+                        if (DEBUG) Log.d(TAG, String.format(
+                                "userSetupComplete = %s, ignoring camera launch gesture.",
+                                userSetupComplete));
+                        break;
+                    }
+
+                    // This gets executed before we will show Keyguard, so post it in order that the
+                    // state is correct.
+                    mMainExecutor.execute(() -> mCommandQueueCallbacks.onCameraLaunchGestureDetected(
+                            StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE));
+                    break;
+                }
             }
             Trace.endSection();
         }
