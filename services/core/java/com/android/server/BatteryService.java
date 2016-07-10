42d9db0f8db5e2408e574fd8ffd4a82482bee10a
thecrazyskull <anaskarbila@aospa.co>
2016-07-10 06:32:58 -0400

    [SQUASH]BatteryService: add dash charging support
    This is a combination of 3 commits.
    This is the 1st commit message:
    
    BatteryService: add dash charging support
    
    DennySPb: port to R
    Change-Id: Iec71bdd4447d9c69fb6ec792d3bc6d29fff040f2
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    This is the commit message #2:
    
    Dash charging: Add the battery percent back
    
    This was left out of the initial bring up. Now it matches the AOSP charging
    string. (see below)
    
    https: //github.com/DirtyUnicorns/android_frameworks_base/blob/p9x/packages/SystemUI/res/values/strings.xml#L959
    Change-Id: I3760c1256c5008c7af2d2d61d44f3a3b759a79e3
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    This is the commit message #3:
    
    base: Remove bool dependency for DASH charging.
    
    We're already checking for the file path to determine if dash charging
    is available. There's no reason for added check. Especially a check that
    really didn't make sense in the first place. At no point are we going to
    turn this off on a device that has support for it.
    
    Change-Id: I881ff0ba3f88da4c85858e6667713e701637348b
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: adapt to A13
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    This is the commit message #4:
    
    base: allow overlaying fast charge sysfs node
    
    * Not all devices support it, and possibly have different sysfs nodes
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Idd16b1f3737d0c4bd774b590cb52662f020ff937
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -17,6 +17,7 @@
 package com.android.server;
 
 import static com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import static com.android.internal.util.kosp.FileUtils.readOneLine;
 import static com.android.server.health.Utils.copyV1Battery;
 
 import android.annotation.Nullable;
@@ -165,6 +166,10 @@ public final class BatteryService extends SystemService {
 
     private boolean mBatteryLevelLow;
 
+    private final String mFastChargeNode;
+    private boolean mDashCharger;
+    private boolean mLastDashCharger;
+
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -226,6 +231,7 @@ public final class BatteryService extends SystemService {
         }
 
         mBatteryInputSuspended = PowerProperties.battery_input_suspended().orElse(false);
+        mFastChargeNode = mContext.getString(com.android.internal.R.string.config_fastChargeSysfsNode);
     }
 
     @Override
@@ -502,7 +508,9 @@ public final class BatteryService extends SystemService {
                         || mHealthInfo.maxChargingCurrentMicroamps != mLastMaxChargingCurrent
                         || mHealthInfo.maxChargingVoltageMicrovolts != mLastMaxChargingVoltage
                         || mHealthInfo.batteryChargeCounterUah != mLastChargeCounter
-                        || mInvalidCharger != mLastInvalidCharger)) {
+                        || mInvalidCharger != mLastInvalidCharger
+                        || mDashCharger != mLastDashCharger)) {
+        mDashCharger = isDashCharger();
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -676,6 +684,7 @@ public final class BatteryService extends SystemService {
             mLastChargeCounter = mHealthInfo.batteryChargeCounterUah;
             mLastBatteryLevelCritical = mBatteryLevelCritical;
             mLastInvalidCharger = mInvalidCharger;
+            mLastDashCharger = mDashCharger;
         }
     }
 
@@ -707,6 +716,7 @@ public final class BatteryService extends SystemService {
                 BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE,
                 mHealthInfo.maxChargingVoltageMicrovolts);
         intent.putExtra(BatteryManager.EXTRA_CHARGE_COUNTER, mHealthInfo.batteryChargeCounterUah);
+        intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
                     + ", info:" + mHealthInfo.toString());
@@ -759,6 +769,10 @@ public final class BatteryService extends SystemService {
         mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
                 android.Manifest.permission.BATTERY_STATS);
         mLastBatteryLevelChangedSentMs = SystemClock.elapsedRealtime();
+    }
+
+    private boolean isDashCharger() {
+        return !mFastChargeNode.isEmpty() && "1".equals(readOneLine(mFastChargeNode));
     }
 
     // TODO: Current code doesn't work since "--unplugged" flag in BSS was purposefully removed.
