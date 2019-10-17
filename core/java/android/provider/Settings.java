c6f704a0b365a3660888f65891e151679f949e45
Ido Ben-Hur <idoybh2@gmail.com>
2019-10-17 04:15:53 +0300

    base: Allow toggling QS footer text [1/2]
    Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5726,6 +5726,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
+        /** 
+         * Whether to show qs footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
