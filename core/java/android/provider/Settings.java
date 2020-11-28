13c3757f310e2e5a829175d96250d004bfd7a1a9
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-28 19:36:51 +0200

    BatteryMeterView: Remove the icon view if we are not using it
    Having the view while not really using it (vis is GONE) is expansive.
    
    Also improve some code while at it:
    * Properly support multi users
    * Use the settings observer instead of tunables
    * Fix AOSP derp for estimates, where it would not be registered for change after a user switch
    * Properly document the settings string
    * Misc improvements
    
    Change-Id: If35b003176234c6a2aef0a4b09ab166d87f6052a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5586,7 +5586,9 @@ public final class Settings {
 
         /**
          * Style of battery to show.
-         *
+         * 0 = protrait (default)
+         * 1 = circle
+         * 2 = text
          * @hide
          */
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
