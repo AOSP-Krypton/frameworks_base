5b3d1fabf7c3d8f7b2bac632ebb27937a87019f2
jhonboy121 <alfredmathew05@gmail.com>
2021-03-12 13:57:11 +0000

    base: added reboot to recovery option in power menu

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsComponent.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsComponent.java
@@ -121,4 +121,12 @@ public class GlobalActionsComponent extends SystemUI implements Callbacks, Globa
         } catch (RemoteException e) {
         }
     }
+
+    @Override
+    public void rebootToRecovery() {
+        try {
+            mBarService.rebootToRecovery();
+        } catch (RemoteException e) {
+        }
+    }
 }
