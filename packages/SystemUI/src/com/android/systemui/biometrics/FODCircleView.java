dbe02c8011513aad76e22ce4b6851765a5eafd9b
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-03-20 22:04:52 +0200

    FODCircleView: Rework icon dimming
    - Switch to Spline interpolation.
    
    idoybh: we don't need to configure it, also adapt to dim animations
    jhonboy121: simplify conversion of integer-array resource to float array
    
    Change-Id: I45bf7d539ef1f9221fc13fa0b042c6ef741d34a3
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -80,6 +80,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.provider.Settings;
+import android.util.Spline;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.WindowManager;
@@ -105,26 +106,6 @@ import java.util.TimerTask;
 
 public class FODCircleView extends ImageView {
     private static final int FADE_ANIM_DURATION = 250;
-    private static final int[][] BRIGHTNESS_ALPHA_ARRAY = {
-        new int[]{0, 255},
-        new int[]{1, 224},
-        new int[]{2, 213},
-        new int[]{3, 211},
-        new int[]{4, 208},
-        new int[]{5, 206},
-        new int[]{6, 203},
-        new int[]{8, 200},
-        new int[]{10, 196},
-        new int[]{15, 186},
-        new int[]{20, 176},
-        new int[]{30, 160},
-        new int[]{45, 139},
-        new int[]{70, 114},
-        new int[]{100, 90},
-        new int[]{150, 56},
-        new int[]{227, 14},
-        new int[]{255, 0}
-    };
     private final int mPositionX;
     private final int mPositionY;
     private final int mSize;
@@ -166,6 +147,7 @@ public class FODCircleView extends ImageView {
     private final FODAnimation mFODAnimation;
     private final KeyguardUpdateMonitor mUpdateMonitor;
     private final LockPatternUtils mLockPatternUtils;
+    private final Spline mSpline;
 
     private final IFingerprintInscreenCallback mFingerprintInscreenCallback =
             new IFingerprintInscreenCallback.Stub() {
@@ -308,6 +290,9 @@ public class FODCircleView extends ImageView {
         mFODAnimation = new FODAnimation(mContext, mWindowManager, mPositionY);
 
         final Resources res = mContext.getResources();
+        mSpline = Spline.createSpline(getFloatArray(res.getIntArray(R.array.config_FODiconDisplayBrightness)),
+            getFloatArray(res.getIntArray(R.array.config_FODiconDimAmount)));
+
         mTargetUsesInKernelDimming = res.getBoolean(com.android.internal.R.bool.config_targetUsesInKernelDimming);
         mNavigationBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
         mPaintFingerprint.setColor(res.getColor(R.color.config_fodColor));
@@ -375,31 +360,17 @@ public class FODCircleView extends ImageView {
         }
     }
 
-    private int interpolate(int i, int i2, int i3, int i4, int i5) {
-        int i6 = i5 - i4;
-        int i7 = i - i2;
-        int i8 = ((i6 * 2) * i7) / (i3 - i2);
-        int i9 = i8 / 2;
-        int i10 = i2 - i3;
-        return i4 + i9 + (i8 % 2) + ((i10 == 0 || i6 == 0) ? 0 : (((i7 * 2) * (i - i3)) / i6) / i10);
+    private float[] getFloatArray(int[] array) {
+        int len = array.length;
+        float[] floatArray = new float[len];
+        for (int i = 0; i < len; i++) {
+            floatArray[i] = (float) array[i];
+        }
+        return floatArray;
     }
 
     private int getDimAlpha() {
-        int currentBrightness = getCurrentBrightness();
-        int length = BRIGHTNESS_ALPHA_ARRAY.length;
-        int i = 0;
-        while (i < length && BRIGHTNESS_ALPHA_ARRAY[i][0] < currentBrightness) {
-            i++;
-        }
-        if (i == 0) {
-            return BRIGHTNESS_ALPHA_ARRAY[0][1];
-        }
-        if (i == length) {
-            return BRIGHTNESS_ALPHA_ARRAY[length - 1][1];
-        }
-        int[][] iArr = BRIGHTNESS_ALPHA_ARRAY;
-        int i2 = i - 1;
-        return interpolate(currentBrightness, iArr[i2][0], iArr[i][0], iArr[i2][1], iArr[i][1]);
+        return Math.round(mSpline.interpolate(getCurrentBrightness()));
     }
 
     private void updateIconDim(boolean animate) {
