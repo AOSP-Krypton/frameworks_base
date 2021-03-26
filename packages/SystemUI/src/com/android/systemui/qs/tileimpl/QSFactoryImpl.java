61d13952be2545cf1c9327e43de1e4369e06b22f
Adnan Begovic <adnan@cyngn.com>
2021-03-26 18:56:46 -0300

    SystemUI: Add AmbientDisplay tile
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Fri Dec 11 16:16:02 2015 -0800
    
        SystemUI: Readd AmbientDisplayTile.
    
        Change-Id: Ie7b93ba0f87d505206b5d60f193c2234d83d7eac
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Sep 17 22:51:15 2017 +0300
    
        SystemUI: Hide ambient display tile if device does not support it
    
        * Add a check similar to the Settings app
    
        Change-Id: I603cd28b8dd1fadff9ffcfb6953c7b72514b5857
    
    Change-Id: Iea339685f523016876f059052e66e97fd5170a81
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.qs.tiles.CaffeineTile;
+import com.android.systemui.qs.tiles.AmbientDisplayTile;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -96,6 +97,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<ReadingModeTile> mReadingModeTileProvider;
     private final Provider<RefreshRateTile> mRefreshRateTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
+    private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -133,7 +135,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<QuickAccessWalletTile> quickAccessWalletTileProvider,
             Provider<ReadingModeTile> readingModeTileProvider,
             Provider<RefreshRateTile> refreshRateTileProvider,
-            Provider<CaffeineTile> caffeineTileProvider) {
+            Provider<CaffeineTile> caffeineTileProvider,
+            Provider<AmbientDisplayTile> ambientDisplayTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -167,6 +170,7 @@ public class QSFactoryImpl implements QSFactory {
         mReadingModeTileProvider = readingModeTileProvider;
         mRefreshRateTileProvider = refreshRateTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
+        mAmbientDisplayTileProvider = ambientDisplayTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -239,6 +243,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mRefreshRateTileProvider.get();
             case "caffeine":
                 return mCaffeineTileProvider.get();
+            case "ambient_display":
+                return mAmbientDisplayTileProvider.get();
         }
 
         // Custom tiles
