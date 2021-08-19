d27febc2580244ba44eb1a9b629f0420a80ab10a
jhonboy121 <alfredmathew05@gmail.com>
2021-08-19 19:22:36 +0530

    Revert "base: don't build accents"
    This reverts commit 06f936dd30eb11725cfd8323a2421a661f583447.

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -17,6 +17,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := frameworks-base-overlays
 LOCAL_REQUIRED_MODULES := \
+	AccentColorBlackOverlay \
+	AccentColorCinnamonOverlay \
+	AccentColorOceanOverlay \
+	AccentColorOrchidOverlay \
+	AccentColorSpaceOverlay \
+	AccentColorGreenOverlay \
+	AccentColorPurpleOverlay \
+	AccentColorPaletteOverlay \
+	AccentColorCarbonOverlay \
+	AccentColorSandOverlay \
+	AccentColorAmethystOverlay \
+	AccentColorAquamarineOverlay \
+	AccentColorTangerineOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
     DisplayCutoutEmulationHoleOverlay \
