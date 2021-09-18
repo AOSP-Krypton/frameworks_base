b66ca9a8cc50d202637d301e8999d348556c743d
Pranav <npv12@iitbbs.ac.in>
2021-09-18 18:31:49 +0530

    Revert "SystemUI: BrightnessSlider: Scale animation duration with defined BrightnessRampRate"
    This reverts commit 5448ccadfa7316bc809fb57c6ecff07de07e46d3.

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -96,7 +96,6 @@ public class BrightnessController implements ToggleSlider.Listener {
     private boolean mControlValueInitialized;
 
     private ValueAnimator mSliderAnimator;
-    private float mBrightnessRampRate;
 
     public interface BrightnessStateChangeCallback {
         public void onBrightnessLevelChanged();
@@ -328,8 +327,6 @@ public class BrightnessController implements ToggleSlider.Listener {
         mDisplayManager = context.getSystemService(DisplayManager.class);
         mVrManager = IVrManager.Stub.asInterface(ServiceManager.getService(
                 Context.VR_SERVICE));
-        mBrightnessRampRate = mContext.getResources().getFloat(
-                com.android.internal.R.dimen.config_brightnessRampRateFastFloat);
     }
 
     public void addStateChangedCallback(BrightnessStateChangeCallback cb) {
@@ -472,8 +469,8 @@ public class BrightnessController implements ToggleSlider.Listener {
             mControl.setValue((int) animation.getAnimatedValue());
             mExternalChange = false;
         });
-        final long animationDuration = Math.round(SLIDER_ANIMATION_DURATION * Math.abs(
-                mControl.getValue() - target)/mBrightnessRampRate) / GAMMA_SPACE_MAX;
+        final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
+                mControl.getValue() - target) / GAMMA_SPACE_MAX;
         mSliderAnimator.setDuration(animationDuration);
         mSliderAnimator.start();
     }
