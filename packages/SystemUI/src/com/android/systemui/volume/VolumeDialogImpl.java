LineageOS/android_frameworks_base

SystemUI: fix volume dialog position on TV
Expanded volume panel didn't respect the TV dimensions.
Bring them back conditionally.

Change-Id: I1d0c884726b74cc38fb825aebdff2484285789c2

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -244,7 +244,11 @@ public class VolumeDialogImpl implements VolumeDialog,
     private void initDialog() {
 
         // Gravitate various views left/right depending on panel placement setting.
-        final int panelGravity = mVolumePanelOnLeft ? Gravity.LEFT : Gravity.RIGHT;
+        int panelGravity =
+                mContext.getResources().getInteger(R.integer.volume_dialog_gravity);
+        if (!mShowActiveStreamOnly) {
+            panelGravity = mVolumePanelOnLeft ? Gravity.LEFT : Gravity.RIGHT;
+        }
 
         mConfigurableTexts = new ConfigurableTexts(mContext);
         mHovering = false;
@@ -291,7 +295,8 @@ public class VolumeDialogImpl implements VolumeDialog,
 
         FrameLayout.LayoutParams dialogViewLP =
                 (FrameLayout.LayoutParams) mDialogView.getLayoutParams();
-        dialogViewLP.gravity = Gravity.CENTER_VERTICAL;
+        dialogViewLP.gravity = mShowActiveStreamOnly ? panelGravity
+                : Gravity.CENTER_VERTICAL;
         mDialogView.setLayoutParams(dialogViewLP);
 
         mDialogMainView = mDialog.findViewById(R.id.main);

AOSP-Krypton/frameworks_base

Signed-off-by: Pranav <npv12@iitbbs.ac.in>

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -237,7 +237,11 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private void initDialog() {
         // Gravitate various views left/right depending on panel placement setting.
-        final int panelGravity = mLeftVolumeRocker ? Gravity.LEFT : Gravity.RIGHT;
+        int panelGravity =
+                mContext.getResources().getInteger(R.integer.volume_dialog_gravity);
+        if (!mShowActiveStreamOnly) {
+            panelGravity = mLeftVolumeRocker ? Gravity.LEFT : Gravity.RIGHT;
+        }
 
         mConfigurableTexts = new ConfigurableTexts(mContext);
         mHovering = false;
@@ -284,7 +288,8 @@ public class VolumeDialogImpl implements VolumeDialog,
 
         FrameLayout.LayoutParams dialogViewLP =
                 (FrameLayout.LayoutParams) mDialogView.getLayoutParams();
-        dialogViewLP.gravity = Gravity.CENTER_VERTICAL;
+        dialogViewLP.gravity = mShowActiveStreamOnly ? panelGravity
+                : Gravity.CENTER_VERTICAL;
         mDialogView.setLayoutParams(dialogViewLP);
 
         mDialogMainView = mDialog.findViewById(R.id.main);