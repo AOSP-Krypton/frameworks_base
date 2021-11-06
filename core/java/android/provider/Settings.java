1561b4fe2afbd0aa40ad4db5b6677dca4f027820
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 14:09:41 +0000

    Make "Require unlocking to use sensitive QS tiles" optional [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I355ef643fc046ce50f40924db130ad212fea5800
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5548,6 +5548,11 @@ public final class Settings {
         public static final String ENABLE_UDFPS_START_HAPTIC_FEEDBACK = "enable_udfps_start_haptic_feedback";
 
         /**
+         * @hide
+         */
+        public static final String USE_TILES_ON_SECURE_KEYGUARD = "use_tiles_on_secure_keyguard";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
