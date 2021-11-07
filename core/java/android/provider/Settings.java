02e00879d44a407fcae78f74fd91c2ed45d3c3fc
beanstown106 <nbenis106@gmail.com>
2021-11-07 14:21:56 +0100

    fwb: Incall vibration options [1/3]
    * This works with google and aosp dialer: )
    Change-Id: Id200af53d5a66d00ec1a420379fcff0adea1f874
    
    Change-Id: Ia5f56a5be7b42438cdad4124b5317e75a1e85ed6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5307,6 +5307,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+        * Whether to enable or disable vibration during calls
+        * @hide
+        */
+        public static final String INCALL_FEEDBACK_VIBRATE = "incall_feeedback_vibrate";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
