0af100d7743bea979b7d01e81905438eefc8979e
Pranav <npv12@iitbbs.ac.in>
2022-06-07 17:13:33 +0530

    pputil: Update raven fp to june

--- core/java/com/android/internal/util/krypton/PixelPropsUtils.java
+++ core/java/com/android/internal/util/krypton/PixelPropsUtils.java
@@ -48,7 +48,7 @@ public final class PixelPropsUtils {
         entry("DEVICE", "raven"),
         entry("PRODUCT", "raven"),
         entry("MODEL", "Pixel 6 Pro"),
-        entry("FINGERPRINT", "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys")
+        entry("FINGERPRINT", "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys")
     );
 
     private static final Map<String, String> marlinProps = Map.ofEntries(
