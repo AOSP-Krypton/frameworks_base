f52208ddfc6a3de07ce845367f203e94a123bac8
Pranav <npv12@iitbbs.ac.in>
2022-06-29 11:08:37 +0530

    pputil: Stop spoofing playstore

--- core/java/com/android/internal/util/kosp/PixelPropsUtils.java
+++ core/java/com/android/internal/util/kosp/PixelPropsUtils.java
@@ -64,7 +64,6 @@ public final class PixelPropsUtils {
     );
 
     private static final List<String> packagesToChange = List.of(
-        "com.android.vending",
         "com.breel.wallpapers20",
         "com.google.android.apps.customization.pixel",
         "com.google.android.apps.fitness",
